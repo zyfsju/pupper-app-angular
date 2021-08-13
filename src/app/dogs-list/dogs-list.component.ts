fd94e886e0a98a485c65363e7b3460caafa75792
Yifan <y_zhou05@outlook.com>
2021-08-13 15:42:35 -0400

    Add files via upload

---
+++ src/app/dogs-list/dogs-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DogService, BreedImagesResponse } from '../dog.service';
+
+@Component({
+  selector: 'app-dogs-list',
+  templateUrl: './dogs-list.component.html',
+  styleUrls: ['./dogs-list.component.css']
+})
+export class DogsListComponent implements OnInit {
+
+  breed: string;
+  subBreed: string;
+  dogUrls: string[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private dogService: DogService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getDogs();
+  }
+
+  getDogs() {
+    this.breed = this.route.snapshot.paramMap.get('breed');
+    this.subBreed = this.route.snapshot.paramMap.get('sub-breed');
+
+    this.dogService.findMany(10, this.breed, this.subBreed)
+    .subscribe((result: BreedImagesResponse) => {
+      if (result.status === "success") {
+        this.dogUrls = result.message;
+      }
+    })
+  }
+
+}
+
